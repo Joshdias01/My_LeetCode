ac2bfb16ee1d3b63a13b09ed1ae58169792e17fe
Josh Dias <joshuadias205@gmail.com>
2026-01-12 19:48:09 +0530

    Modify CountNumbersUsingLog.java file

---
+++ src/Math/CountNumbersUsingLog.java
@@ -0,0 +1,9 @@
+package Math;
+
+public class CountNumbersUsingLog {
+    public static void main(String[] args) {
+        int n1=12684231;
+        int count = (int) (Math.log10(n1) + 1);
+        System.out.println(count);
+    }
+}
