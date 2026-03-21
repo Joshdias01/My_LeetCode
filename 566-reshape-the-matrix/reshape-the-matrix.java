b856f0f949c60acd7b8d7d4101c96f7dd2826caa
Joshua dias <joshuadias205@gmail.com>
2026-03-22 00:25:00 +0530

    Time: 3 ms (3.51%) | Memory: 47.3 MB (73.39%) - LeetSync

---
+++ 566-reshape-the-matrix/reshape-the-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int[][] ans = new int[r][c];
+        if(mat.length*mat[0].length!=(r*c))
+            return mat;
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                list.add(mat[i][j]);
+            }
+        }
+        int idx=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                ans[i][j]=list.get(idx++);
+            }
+        }
+        return ans;
+    }
+}
