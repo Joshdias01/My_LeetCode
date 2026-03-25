d03bfbb2cdacf33da0ef7f0932ac4cfffc360db8
Joshua dias <joshuadias205@gmail.com>
2026-03-25 23:59:46 +0530

    Time: 34 ms (70.06%) | Memory: 54.2 MB (87.01%) - LeetSync

--- 1533-display-table-of-food-orders-in-a-restaurant/display-table-of-food-orders-in-a-restaurant.java
+++ 1533-display-table-of-food-orders-in-a-restaurant/display-table-of-food-orders-in-a-restaurant.java
@@ -1,8 +1,8 @@
 class Solution {
     public List<List<String>> displayTable(List<List<String>> orders) {
         List<List<String>> res = new ArrayList<>();
-        TreeSet<String> set = new TreeSet<>();
-        TreeMap<Integer,Map<String,Integer>> map = new TreeMap<>();
+        TreeSet<String> set = new TreeSet<>(); // unique item
+        TreeMap<Integer,Map<String,Integer>> map = new TreeMap<>(); //table->(item->count)
         
         for(List s:orders){
             set.add(s.get(2).toString());
@@ -13,27 +13,13 @@ class Solution {
             Map<String,Integer> mp=map.get(Integer.valueOf(s.get(1).toString()));
             mp.put(s.get(2).toString(),mp.getOrDefault(s.get(2).toString(),0)+1);
         }
-        System.out.println(map);
+
         ArrayList<String> arr = new ArrayList<>();
         arr.add("Table");
         for(String s:set)
             arr.add(s);
         res.add(arr);
 
-        for(Map.Entry<Integer,Map<String,Integer>> entry : map.entrySet()){
-            arr=new ArrayList<>();
-            arr.add(entry.getKey().toString());
-            Map<String,Integer> mp = entry.getValue();
-            for(String s:res.get(0)){
-                if(mp.containsKey(s)){
-                    arr.add(mp.get(s).toString());
-                }
-                else
-                    arr.add("0");
-            }
-            arr.remove(1);
-            res.add(arr);
-        }
         // for(String s:table){
         //     arr=new ArrayList<>();
         //     arr.add(s);
@@ -46,6 +32,21 @@ class Solution {
         //     arr=new ArrayList<>();
         //     arr.add()
         // }
+        for(Map.Entry<Integer,Map<String,Integer>> entry : map.entrySet()){
+            arr=new ArrayList<>();
+            arr.add(entry.getKey().toString());
+            Map<String,Integer> mp = entry.getValue();
+            for(String s:res.get(0)){
+                if(mp.containsKey(s)){
+                    arr.add(mp.get(s).toString());
+                }
+                else
+                    arr.add("0");
+            }
+            // System.out.print(arr);
+            arr.remove(1); // i should remove that one unnecessary no;
+            res.add(arr);
+        }
         return res;
     }
 }
