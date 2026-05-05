e5495e614f4db660e2e9d529aeec57284b6b91f2
Joshua dias <joshuadias205@gmail.com>
2026-05-05 17:52:14 +0530

    Time: 3 ms (78.58%) | Memory: 43.8 MB (55.42%) - LeetSync

---
+++ 2235-capitalize-the-title/capitalize-the-title.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String capitalizeTitle(String title) {
+        String[] words = title.split(" ");
+        StringBuilder ans = new StringBuilder();
+        for(int i=0;i<words.length;i++){
+            words[i]=words[i].toLowerCase();
+            if(words[i].length()>2){
+                ans.append(Character.toUpperCase(words[i].charAt(0)));
+                ans.append(words[i].substring(1));
+            }else{
+                ans.append(words[i]);
+            }
+            if (i != words.length - 1) {
+                ans.append(" ");
+            }
+        }
+        return ans.toString();
+    }
+}
