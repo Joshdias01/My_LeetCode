e78a3f86744b97f9e8ebcf9b66f8748f7a15c57a
Joshua dias <joshuadias205@gmail.com>
2026-05-24 15:27:37 +0530

    Time: 20 ms (12.31%) | Memory: 45.2 MB (92.64%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int i=0,k=nums.length-1;
+        int diff=nums[0]+nums[1]+nums[2];
+        while(i<nums.length){
+            int j=i+1;
+            k=nums.length-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==target) return sum;
+                int mod=Math.abs(sum-target);
+                if(Math.abs(sum-target)<Math.abs(diff-target)){
+                    diff=sum;
+                }
+                if (sum < target) {
+                    j++; 
+                } else {
+                    k--;
+                }
+            }
+            i++;
+        }
+        return diff;
+    }
+}
