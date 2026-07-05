cbcddef839dd0a5dc64865fcb19ab838d4bbd9e2
Joshua dias <joshuadias205@gmail.com>
2026-07-05 23:30:52 +0530

    Time: 157 ms (9.08%) | Memory: 101.2 MB (9.18%) - LeetSync

---
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minSpeedOnTime(int[] nums, double hour) {
+        int low=1,high=10000000;
+        for(int i:nums){
+            high=Math.max(i,high);
+        }
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(check(nums,mid,hour)){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    boolean check(int[] nums,int mid,double hour){
+        double hr=0;
+        for(int i=0;i<nums.length;i++){
+            if(i!=nums.length-1){
+                hr+=Math.ceil((double)nums[i]/mid);
+            }else{
+                hr+=((double)nums[i]/(double)mid);
+            }
+        }
+        System.out.println(mid+" "+hr);
+        return (hr<=hour);
+    }
+}
