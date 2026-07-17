5065fd8ba7add8dc1f4370298cfe48a969482722
Joshua dias <joshuadias205@gmail.com>
2026-07-17 10:33:23 +0530

    Time: 2549 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,8 @@
+with ids as(
+    select sales_id
+    from orders 
+    where com_id=(select com_id from company where name='RED')
+)
+select name 
+from salesPerson 
+where sales_id not in (select sales_id from ids);
